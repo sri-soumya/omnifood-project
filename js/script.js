d89632b2dd3597bf48e8c45292c28b1309ac4307
Soumya Srivastava <sri.soumya0607@gmail.com>
2021-11-13 19:06:33 +0530

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -9,6 +9,53 @@ mobileBtn.addEventListener("click", function () {
   header.classList.toggle("nav-open");
 });
 
+/////////////SMOOTH SCROLLING
+
+const links = document.querySelectorAll("a:link");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else if (href.startsWith("#")) {
+      const sectionel = document.querySelector(href);
+      sectionel.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-item"))
+      header.classList.toggle("nav-open");
+  });
+});
+
+// const allLinks = document.querySelectorAll("a:link");
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
+
+//     // Close mobile naviagtion
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
